c2a152cf663deb5bf93f125076b93f4c59707050
Alexandre Diaz Codina <alexandre.diaz.codina@gmail.com>
2021-08-14 17:24:15 +0200

    no inputs management

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ params = {
 if requests.get(url,params=params).json()["detail"][0]["type"] == "value_error.missing":
     st.write('Please input the values in the sidebar to receive an estimate of your fare costs')
 
-else:
+elif isinstance(requests.get(url,params=params).json()["prediction"],float):
     response = requests.get(url,params=params)
     st.write('The estimated cost of your ride is: ', round(response.json()["prediction"],2), '$')
     
@@ -76,5 +76,7 @@ else:
     df = df.append(new_row, ignore_index=True)
 
     st.map(df)
-        
+    
+else:
+    st.write('Please input the values in the sidebar to receive an estimate of your fare costs')
 
