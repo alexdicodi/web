066d91619a273ccf198b82d7b0102603b0a69204
Alexandre Diaz Codina <alexandre.diaz.codina@gmail.com>
2021-08-14 12:06:56 +0200

    map is working

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+from numpy.core.fromnumeric import mean
 import streamlit as st
 import requests
+import numpy as np
+import pandas as pd
+import pydeck as pdk
 
 #Setting the global params
 
@@ -51,26 +55,42 @@ params = {
     "passenger_count": pass_count_input
 }
 
+#We will now call our API endpoint and return the prediction to the user
+
 response = requests.get(url,params=params)
-st.write(response.json())
+st.write('The estimated cost of your ride is: ', round(response.json()["prediction"],2), '€')
 
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
+#Will play around with maps
 
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
 
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
-
-
-
-'''
-
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-'''
+st.pydeck_chart(pdk.Deck(
+     map_style='mapbox://styles/mapbox/light-v9',
+     initial_view_state=pdk.ViewState(
+         latitude=37.76,
+         longitude=-122.4,
+         zoom=11,
+         pitch=50,
+     ),
+     layers=[
+         pdk.Layer(
+            'HexagonLayer',
+            data=df,
+            get_position='[lon, lat]',
+            radius=200,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+            pickable=True,
+            extruded=True,
+         ),
+         pdk.Layer(
+             'ScatterplotLayer',
+             data=df,
+             get_position='[lon, lat]',
+             get_color='[200, 30, 0, 160]',
+             get_radius=200,
+         ),
+     ],
+ ))
